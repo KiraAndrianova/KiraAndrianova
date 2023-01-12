4aeae6ef15be853c08425938b7f4f93ce0f465ef
Kira Andrianova <kira_andrianova@epam.com>
2023-01-12 15:10:17 +0300

    Homework 4 changes

--- src/test/java/com/epam/tc/hw4/BaseTest.java
+++ src/test/java/com/epam/tc/hw4/BaseTest.java
@@ -1,14 +1,10 @@
 package com.epam.tc.hw4;
 
-import com.epam.tc.hw4.attachmentutils.AttachmentUtils;
 import com.epam.tc.hw4.driverutils.DriverManager;
 import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
-import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
 
